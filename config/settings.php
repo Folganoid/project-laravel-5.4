54275ba78461205a29e8057c0ce102ab972d8087
Andrey <folganoid@yahoo.com>
2017-09-23 19:26:53 +0300

    reload

---
+++ config/settings.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fg
+ * Date: 23.09.17
+ * Time: 19:24
+ */
--- routes/web.php
+++ routes/web.php
@@ -11,60 +11,6 @@
 |
 */
 
-/*
 Route::get('/', function () {
     return view('welcome');
 });
-*/
-
-Route::group(['middleware'=> 'web'], function() {
-
-    Route::match(['get', 'post'], '/', ['uses'=>'IndexController@execute', 'as'=>'home']);
-    Route::get('/page/{alias}', ['uses'=>'PageController@execute', 'as'=>'page']);
-
-    Route::auth();
-
-
-});
-
-/**
- * Admin service
- */
-Route::group(['prefix'=>'admin', 'middleware'=>'auth'], function() {
-
-
-    //admin
-    Route::get('/', function() {
-
-
-    });
-
-    //admin/pages
-    Route::group(['prefix'=>'pages'], function() {
-
-        Route::get('/', ['uses'=>'PagesController@execute', 'as'=>'pages']);
-        Route::match(['get', 'post'], '/add', ['uses'=>'PagesAddController@execute', 'as'=>'pagesAdd']);
-        Route::match(['get', 'post', 'delete'], '/edit/{page}', ['uses'=>'PagesEditController@execute', 'as'=>'pagesEdit']);
-
-    });
-
-    //portfolios/pages
-    Route::group(['prefix'=>'portfolios'], function() {
-
-        Route::get('/', ['uses'=>'PortfoliosController@execute', 'as'=>'portfolios']);
-        Route::match(['get', 'post'], '/add', ['uses'=>'PortfoliosAddController@execute', 'as'=>'PortfoliosAdd']);
-        Route::match(['get', 'post', 'delete'], '/edit/{portfolios}', ['uses'=>'PortfoliosEditController@execute', 'as'=>'PortfoliosEdit']);
-
-    });
-
-    //services/pages
-    Route::group(['prefix'=>'services'], function() {
-
-        Route::get('/', ['uses'=>'ServicesController@execute', 'as'=>'services']);
-        Route::match(['get', 'post'], '/add', ['uses'=>'ServicesAddController@execute', 'as'=>'ServicesAdd']);
-        Route::match(['get', 'post', 'delete'], '/edit/{services}', ['uses'=>'ServicesEditController@execute', 'as'=>'ServicesEdit']);
-
-    });
-});
-
-
