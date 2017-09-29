ad3bde35924bc5eab47f576354c0fee424b46a4c
Andrey <folganoid@yahoo.com>
2017-09-29 19:23:25 +0300

    create rightbar

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -4,6 +4,7 @@ namespace Corp\Http\Controllers;
 
 use Corp\Menu;
 use Corp\Repositories\ArticlesRepository;
+use Corp\Repositories\CommentsRepository;
 use Corp\Repositories\MenusRepository;
 use Corp\Repositories\PortfoliosRepository;
 use Illuminate\Http\Request;
@@ -11,12 +12,13 @@ use Config;
 
 class ArticlesController extends SiteController
 {
-    public function __construct(PortfoliosRepository $p_rep, ArticlesRepository $a_rep)
+    public function __construct(PortfoliosRepository $p_rep, ArticlesRepository $a_rep, CommentsRepository $c_rep)
     {
         parent::__construct(new MenusRepository(new Menu));
 
         $this->p_rep = $p_rep;
         $this->a_rep = $a_rep;
+        $this->c_rep = $c_rep;
         $this->bar = 'right';
         $this->template = env('THEME').'.articles';
     }
@@ -29,8 +31,21 @@ class ArticlesController extends SiteController
         $content = view(env('THEME').'.articles_content')->with('articles', $articles)->render();
         $this->vars = array_add($this->vars, 'content', $content);
 
+        $comments = $this->getComments(config('settings.recent_comments'));
+        $portfolios = $this->getPortfolios(config('settings.recent_portfolios'));
+        $this->contentRightBar = view(env('THEME').'.articlesBar')->with(['comments' => $comments, 'portfolios' => $portfolios]);
 
         return $this->renderOutput();
+    }
+
+    public function getComments($take) {
+        $comments = $this->c_rep->get(['text', 'name', 'email', 'site', 'article_id', 'user_id'], $take);
+        return $comments;
+    }
+
+    public function getPortfolios($take) {
+        $portfolios = $this->p_rep->get(['title', 'text', 'alias', 'customer', 'img', 'filter_alias'], $take);
+        return $portfolios;
     }
 
     public function getArticles($alias = FALSE)
--- config/settings.php
+++ config/settings.php
@@ -12,6 +12,9 @@ return
         'hom_port_count' => 5,
         'home_articles_count' => 3,
         'paginate' => 2,
+        'recent_comments' => 3,
+        'recent_portfolios' => 3,
+
     ];
 
 ?>
